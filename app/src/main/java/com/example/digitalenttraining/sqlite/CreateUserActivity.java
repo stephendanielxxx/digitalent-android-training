bd59a1bd9b7c2e5505ad7a5f587cc28bcb6629ea
teke <fatih.avila@gmail.com>
2021-03-04 14:23:33 +0700

    Day 3 - sqlite by fatih

--- app/src/main/java/com/example/digitalenttraining/sqlite/CreateUserActivity.java
+++ app/src/main/java/com/example/digitalenttraining/sqlite/CreateUserActivity.java
@@ -15,25 +15,28 @@ public class CreateUserActivity extends AppCompatActivity {
     Button btnCreateUser;
     EditText etIdUser, etNameUser;
     DataHelper dataHelper;
-    String id, name;
+    String name;
+    int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
+        dataHelper = new DataHelper(this);
 
         btnCreateUser = findViewById(R.id.btnCreateUser);
         etIdUser = findViewById(R.id.etIdUser);
         etNameUser = findViewById(R.id.etNameUser);
 
-        id = etIdUser.getText().toString();
-        name = etNameUser.getText().toString();
 
         btnCreateUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                id = Integer.parseInt(etIdUser.getText().toString());
+                name = etNameUser.getText().toString();
+
                 SQLiteDatabase db = dataHelper.getWritableDatabase();
-                db.execSQL("insert into user(id,name) values('"+id+"','"+name+"')");
+                db.execSQL("insert into user(id,name) values("+id+",'"+name+"')");
                 Toast.makeText(CreateUserActivity.this, "Data berhasil ditambahkan.",
                         Toast.LENGTH_SHORT).show();
                 MainSqliteActivity.main.refreshDataList();
